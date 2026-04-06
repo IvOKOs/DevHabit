9db72dabce0ea69bf216ebe5c88b6b50bb55eea6
Ivo Kostadinov <ivaylo.kostadinov98@gmail.com>
2026-04-06 16:48:52 +0300

    Added initial things for hypermedia support

---
+++ DevHabit.Api/Dtos/Common/LinkDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DevHabit.Api.Dtos.Common;
+
+public sealed record LinkDto
+{
+    public string Href { get; init; }
+    public string Rel {  get; init; }
+    public string Method { get; init; }
+}
---
+++ DevHabit.Api/Service/LinkService.cs
@@ -0,0 +1,31 @@
+﻿using DevHabit.Api.Dtos.Common;
+using Microsoft.AspNetCore.Http;
+
+namespace DevHabit.Api.Service;
+
+// encapsulates what's required to create LinkDto
+public sealed class LinkService(LinkGenerator linkGenerator, IHttpContextAccessor httpContextAccessor)
+{
+    public LinkDto Create(
+        string endpointName,
+        string rel,
+        string method,
+        object? values = null, // the values you can place in the route
+        string? controller = null) // name of controller - useful if you want to reference action
+                                   // that is outside the scope of current controller represented
+                                   // by the current http context
+    {
+        string href = linkGenerator.GetUriByAction(
+            httpContextAccessor.HttpContext!,
+            endpointName,
+            controller,
+            values);
+
+        return new LinkDto
+        {
+            Href = href ?? throw new Exception("Invalid endpoint name provided."),
+            Rel = rel,
+            Method = method,
+        };
+    } 
+}
